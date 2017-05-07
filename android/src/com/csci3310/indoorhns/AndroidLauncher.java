c42e464c4bf75055a2c266ed2ad03cb4eb3c8838
edmundyipll <edmundyipll@hotmail.com>
2017-05-08 00:26:31 +0800

    init commit

---
+++ android/src/com/csci3310/indoorhns/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.csci3310.indoorhns;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.csci3310.indoorhns.IndoorHideAndSeek;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IndoorHideAndSeek(), config);
+	}
+}
