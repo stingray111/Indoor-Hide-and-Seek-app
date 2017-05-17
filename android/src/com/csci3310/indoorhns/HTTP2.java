6454c1b3162edc30c0db0d2a71aec40232c710ac
adigho <adigho2786@gmail.com>
2017-05-17 16:37:17 +0800

    shidanla

---
+++ android/src/com/csci3310/indoorhns/HTTP2.java
@@ -0,0 +1,24 @@
+//package com.csci3310.indoorhns;
+//
+//import retrofit2.Retrofit;
+//import retrofit2.http.GET;
+//import retrofit2.http.POST;
+//
+///**
+// * Created by Igho on 17/5/2017.
+// */
+//
+//public interface HTTP2 {
+//    @GET("/track")
+//    Call<ABC> getXXXX(DEF req);
+//
+//    Retrofit retrofit = new Retrofit.Builder()
+//            .baseUrl("https://ml.internalpositioning.com/")
+//            .build();
+//
+//    HTTP2 service = retrofit.create(GitHubService.class);
+//    Call<ABC> call = service.getXXXX(fdjsfk);
+//    call
+//
+//
+//}
---
+++ android/src/com/csci3310/indoorhns/WifiScanReceiver.java
@@ -0,0 +1,8 @@
+package com.csci3310.indoorhns;
+
+/**
+ * Created by Igho on 17/5/2017.
+ */
+
+public class WifiScanReceiver {
+}
