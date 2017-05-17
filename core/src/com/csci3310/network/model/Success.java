f459e9dc85f55763054f04415dd1c0a425a6017a
Ray LUK <pingshan1013@hotmail.com>
2017-05-17 15:55:40 +0800

    added HTTP class and corresponding model

---
+++ core/src/com/csci3310/network/model/Success.java
@@ -0,0 +1,13 @@
+package com.csci3310.network.model;
+
+/**
+ * Created by ray on 17/5/2017.
+ */
+
+public class Success {
+    public boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+}
